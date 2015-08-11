2f6dc1708010adc34916fba34d747a3db8883154
Phillip Birtcher <philbirt@gmail.com>
2015-08-11 14:48:41 -0500

    Solve bob problem for elixir

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,16 @@
 defmodule Teenager do
   def hey(input) do
-    cond do
+    input = String.replace(input, ~r/\s/, "")
 
+    cond do
+      String.length(input) == 0 ->
+        "Fine. Be that way!"
+      String.at(input, -1) == "?" ->
+        "Sure."
+      String.upcase(input) == input and String.downcase(input) != input ->
+        "Whoa, chill out!"
+      true ->
+        "Whatever."
     end
   end
 end
